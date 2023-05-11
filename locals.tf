62262923d91211de0d5a45ba8f9f9c283a3bef33
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-11 19:58:18 +0530

    auto_scaling

--- locals.tf
+++ locals.tf
@@ -7,9 +7,9 @@ locals {
   }
   all_tags = [
     { key = "env" , value = var.env } ,
-    { key = "project" , value = roboshop1 },
-    { key = "business_unit" , value = ecommerce },
-    { key = "owner" , value = ecommerce_robot },
+    { key = "project" , value = "roboshop1" },
+    { key = "business_unit" , value = "ecommerce" },
+    { key = "owner" , value = "ecommerce_robot" },
     { key = "Name" , value = "${var.env}-${var.component}" }
   ]
 }
