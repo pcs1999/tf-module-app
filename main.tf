f0abb85c677c29caf0c018c8ef28b542b1852162
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-12 13:52:51 +0530

    nexus_pass

--- main.tf
+++ main.tf
@@ -42,9 +42,10 @@ resource "aws_iam_policy" "aws_parameter_policy" {
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource" : ["arn:aws:ssm:us-east-1:490686900756:parameter/${var.env}.${var.component}*",
-                      "arn:aws:ssm:us-east-1:490686900756:parameter/nexus*"
-                    ]
+        "Resource" : [
+          "arn:aws:ssm:us-east-1:490686900756:parameter/${var.env}.${var.component}*",
+          "arn:aws:ssm:us-east-1:490686900756:parameter/nexus*"
+        ]
       },
       {
         "Sid": "VisualEditor1",
