0e7fc91ad9d5dfe3f6c4b75c9da822f5f85bae0f
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-13 12:57:01 +0530

    acces for parameter modification

--- main.tf
+++ main.tf
@@ -45,7 +45,8 @@ resource "aws_iam_policy" "aws_parameter_policy" {
         ],
         "Resource" : [
           "arn:aws:ssm:us-east-1:490686900756:parameter/${var.env}.${var.component}*",
-          "arn:aws:ssm:us-east-1:490686900756:parameter/nexus*"
+          "arn:aws:ssm:us-east-1:490686900756:parameter/nexus*",
+          "arn:aws:ssm:us-east-1:490686900756:parameter/*DOCDB_URL*"
         ]
       },
       {
