2519e294fd8597746b9520733e5dba1eeabacf37
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-14 21:02:50 +0530

    rules_added_lb

--- main.tf
+++ main.tf
@@ -177,3 +177,21 @@ resource "aws_lb_target_group" "target_group" {
 
   }
 }
+
+
+resource "aws_lb_listener_rule" "backend_listener_ruke" {
+  count = var.listener_priority != 0 ? 1 : 0
+  listener_arn = var.listener
+  priority     = var.listener_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.component}-${var.env}.chandupcs.online"]
+    }
+  }
+}
